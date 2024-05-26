e0ca3739fb549bf098221dd04b879c794bec326c
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-05-26 17:02:42 +0200

    Adds setError and setSuccess functions

--- script.js
+++ script.js
@@ -24,40 +24,40 @@ function validateInput() {
   // Checking if a string is empty
   if (firstNameVal === '') {
     success = false;
-    setError();
+    setError(firstName, 'First Name is required');
   } else {
-    setSuccess();
+    setSuccess(firstName);
   };
 
   if (lastNameVal === '') {
     success = false;
-    setError();
+    setError(lastName, 'Last Name is required');
   } else {
-    setSuccess();
+    setSuccess(las);
   };
 
   if (emailVal === '') {
     success = false;
-    setError();
+    setError(email, 'Email is required');
   } else {
-    setSuccess();
+    setSuccess(email);
   };
 
   if (passwordVal === '') {
     success = false;
-    setError();
+    setError(password, 'Password is required');
   } else {
-    setSuccess();
+    setSuccess(password);
   };
 
   if (cpasswordVal === '') {
     success = false;
-    setError();
+    setError(cpassword, 'Confirm Password is required');
   } else if (cpasswordVal !== passwordVal) {
     success = false;
-    SetError();
+    SetError(cpassword, 'Password does not match');
   } else {
-    setSuccess();
+    setSuccess(cpassword);
   };
 
   return success;
@@ -65,5 +65,19 @@ function validateInput() {
 
 function setError(element, message) {
     const inputGroup = element.parentElement;
-    const errorElement = inputGroup
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = message;
+    inputGroup.classList.add('error');
+    inputGroup.classList.remove('success')
+}
+
+function setSuccess(element) {
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('error');
+
+    errorElement.innerText = '';
+    inputGroup.classList.add('success');
+    inputGroup.classList.remove('error');
+
 }
